16bd440579c15da7d46d13b3b33f40e3889b29ed
Konrad Karczewski <kkarczewski94@gmail.com>
2020-11-11 13:41:20 +0100

    Connected Display with store

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { default as bemCssModules } from 'bem-css-modules';
 
 import { default as DisplayStyles } from './Display.module.scss';
 
 const style = bemCssModules(DisplayStyles);
 
+interface RootState {
+  Display: string
+}
+
 const Display: React.FC = () => {
+  const displayValue = useSelector((state: RootState) => state.Display);
   return (
-    <div className={style()}>0</div>
+    <div className={style()}>{displayValue}</div>
   );
 }
 
