f1ef4dbaf044d4aeceebe9ccdb9b4690fbb4523b
Konrad Karczewski <kkarczewski94@gmail.com>
2020-11-13 12:21:49 +0100

    RootStore interface

---
+++ src/_redux/stores/Interfaces/IRootStore.ts
@@ -0,0 +1,7 @@
+export interface IRootStore {
+  CurrentOperation: string | null,
+  IsNewOperationClicked: boolean,
+  DisplayValue: string,
+  PrevValue: string | null
+  ResultValue: string | null
+}
