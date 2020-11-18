0bb17cb2258fa4f47288fdbda9891ca8835600a5
Konrad Karczewski <kkarczewski94@gmail.com>
2020-11-18 10:13:03 +0100

    Added SubDisplay

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -6,6 +6,7 @@ import { default as DisplayStyles } from './Display.module.scss';
 
 import { useSelector } from 'react-redux';
 import { IRootStore } from '../../_redux/stores/Interfaces/IRootStore';
+import SubDisplay from './SubDisplay/SubDisplay';
 
 const style = bemCssModules(DisplayStyles);
 
@@ -14,7 +15,10 @@ const Display: React.FC = () => {
   const displayValue = useSelector((store: IRootStore) => store.DisplayValue);
 
   return (
-    <div className={style()}>{displayValue}</div>
+    <div className={style()}>
+      <SubDisplay />
+      <p className={style('p')}>{displayValue}</p>
+    </div>
   );
 }
 
