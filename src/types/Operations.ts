c8bbc106d99f4c67f41722dbc7ff0dd3292e5791
Konrad Karczewski <kkarczewski94@gmail.com>
2020-11-29 11:54:44 +0100

    First version of operations

---
+++ src/types/Operations.ts
@@ -0,0 +1,11 @@
+export enum Operations {
+  addition = '+',
+  subtraction = '-',
+  multiplication = '*',
+  division = '/',
+  negation = '+/-',
+  squareRoot = 'sqrt',
+  percent = '%',
+  invertion = '1/x',
+  squaredPower = 'x2',
+}
