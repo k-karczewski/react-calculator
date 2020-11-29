a18838ee50f45b0abbbf6d879852d828fe722cc5
Konrad Karczewski <kkarczewski94@gmail.com>
2020-11-29 11:54:09 +0100

    Handlers for special operations

--- src/_redux/reducers/OperationsReducer.ts
+++ src/_redux/reducers/OperationsReducer.ts
@@ -1,6 +1,9 @@
 //actions import
 import { RESET_CALCULATOR } from '../actions/CalculatorActions';
-import { SET_NEW_OPERATION, SET_EQUALS_CLICKED, UNSET_EQUALS_CLICKED, UNSET_NEW_OPERATION_CLICKED } from '../actions/OperationsActions';
+import {
+  SET_NEW_OPERATION, SET_EQUALS_CLICKED, UNSET_EQUALS_CLICKED, UNSET_NEW_OPERATION_CLICKED,
+  SET_SPECIAL_OPERATION_CLICKED, UNSET_SPECIAL_OPERATION_CLICKED
+} from '../actions/OperationsActions';
 
 //interfaces import
 import { IOperationState } from './Interfaces/ICalculatorReducers';
@@ -10,12 +13,13 @@ const defaultState: IOperationState = {
   equalsClicked: false,
   newOperationClicked: false,
   operation: null,
+  specialOperationClicked: false
 };
 
 export const OperationsReducer = (state: IOperationState = defaultState, action: IOperationAction): IOperationState => {
   switch (action.type) {
     case SET_NEW_OPERATION:
-      if (action.payload) return { equalsClicked: false, newOperationClicked: true, operation: action.payload?.operation }
+      if (action.payload) return { ...state, equalsClicked: false, newOperationClicked: true, operation: action.payload?.operation }
       else return { ...state, equalsClicked: false, newOperationClicked: true };
 
     case SET_EQUALS_CLICKED:
@@ -26,6 +30,12 @@ export const OperationsReducer = (state: IOperationState = defaultState, action:
 
     case UNSET_NEW_OPERATION_CLICKED:
       return { ...state, equalsClicked: false, newOperationClicked: false };
+
+    case SET_SPECIAL_OPERATION_CLICKED:
+      return { ...state, specialOperationClicked: true }
+
+    case UNSET_SPECIAL_OPERATION_CLICKED:
+      return { ...state, specialOperationClicked: false }
 
     case RESET_CALCULATOR: return defaultState;
     default: return state;
