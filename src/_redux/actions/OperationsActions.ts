0ea84b0e0980fb435120d626e3f2ef2b57948bba
Konrad Karczewski <kkarczewski94@gmail.com>
2020-12-05 19:24:24 +0100

    Removed unnecessary actions

--- src/_redux/actions/OperationsActions.ts
+++ src/_redux/actions/OperationsActions.ts
@@ -1,6 +1,5 @@
 import { IOperationAction } from "./Interfaces/ICalculatorActions";
 
-export const CLEAR_OPERATION = 'CLEAR_OPERATION';
 export const SET_NEW_OPERATION = 'SET_NEW_OPERATION';
 
 export const UNSET_NEW_OPERATION_CLICKED = 'UNSET_NEW_OPERATION_CLICKED';
@@ -10,10 +9,6 @@ export const UNSET_EQUALS_CLICKED = 'UNSET_EQUALS_CLICKED';
 
 export const SET_SPECIAL_OPERATION_CLICKED = 'SET_SPECIAL_OPERATION_CLICKED';
 export const UNSET_SPECIAL_OPERATION_CLICKED = 'UNSET_SPECIAL_OPERATION_CLICKED';
-
-export const clearOperationAction: IOperationAction = {
-  type: CLEAR_OPERATION
-};
 
 export const setEqualsClickedAction: IOperationAction = {
   type: SET_EQUALS_CLICKED
